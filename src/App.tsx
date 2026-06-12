af8b4973ee2c269d63997f75f3d2ec7ffb3879ef
Lokesh <lenkalokesh98@gmail.com>
2026-06-12 18:57:08 +0530

    feat: update CSS variables and add Raleway font
    - Added Raleway font to the project.
    - Updated CSS variables for theme customization, including new font settings and color variables.
    - Improved formatting for better readability in the CSS file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/landing/HomePage";
 import { Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Timeline from "./pages/landing/timeline/Timeline";
 
 function App() {
   const location = useLocation();
   return (
     <Routes location={location} key={location.pathname}>
-      <Route path="/" element={<HomePage />} />;
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/timeline" element={<Timeline />} />
+      </Route>
     </Routes>
   );
 }
